6cd4c0b5c506638c99fe60c517e48778001cd765
hemant jadhav <hemantjadhav7477@gmail.com>
2024-10-08 16:34:30 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,11 @@
+// echo "# RestApisForTesting" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/JadhavHemant/RestApisForTesting.git
+// git push -u origin main
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/JadhavHemant/RestApisForTesting.git
+// git branch -M main
+// git push -u origin main
